866ae0587a78090a61f7dee034c6f034ebf69cd0
wint <wintcred@gmail.com>
2021-06-05 23:56:38 +0530

    Created Home and NavBar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home></Home>
       </div>
     </div>
   );
